894a57e57f13a8d05df0559a6219d7d87ad41198
balthazar <balthazar.de.moncuit@gmail.com>
2020-04-29 10:23:00 +0200

    cleanup

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserListComponent } from './user-list/user-list.component';
 import { MaterialModule } from '../material.module';
-import { RoundUpPipe } from '../round-up.pipe';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { CoreModule } from '../core/core.module';
 
 @NgModule({
-  declarations: [UserListComponent, RoundUpPipe],
+  declarations: [UserListComponent],
   imports: [
     CoreModule,
     CommonModule,
